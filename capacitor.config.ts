05b3893cf9c802ca797a9f59116532150a6dfbab
andre james <work.andrejames@gmail.com>
2026-02-10 14:13:34 +0800

    android app implementation

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.robertosrework.app',
+  appName: 'Robertos Rework',
+  webDir: 'out'
+};
+
+export default config;
